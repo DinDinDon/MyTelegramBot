c00c5542bb70a3b74d68a91c0d5a9fad1542a5c6
DinDinDon <artak_mar.ru@mail.ru>
2020-05-31 18:25:44 +0300

    Wait

--- src/main/java/ru/Artak/Main.java
+++ src/main/java/ru/Artak/Main.java
@@ -12,64 +12,16 @@ import java.util.*;
 
 public class Main {
 
-    private final HttpClient httpClient = HttpClient.newBuilder()
-            .version(HttpClient.Version.HTTP_1_1)
-            .build();
-
     public static void main(String[] args) throws IOException, InterruptedException {
-        Main main = new Main();
-//
-        main.sendGET();
-//        main.sendMes();
+
+        Bot bot = new Bot();
+        bot.sendGET();
+
+
 
     }
-//    private void sendMes() throws IOException, InterruptedException {
-//        HttpRequest request = null;
-//        request = HttpRequest.newBuilder()
-//                .GET()
-//                .uri(URI.create("https://api.telegram.org/bot1263503443:AAG5Dz5XzWNjscLVjgl_kDEJqLx4zUlcJy8/sendMessage?chat_id=243787596&text=hello"))
-//                .build();
-//        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-//    }
-
-    private void sendGET() throws IOException, InterruptedException {
-        ObjectMapper mapper = new ObjectMapper();
-        HttpRequest request = null;
-        int count = 0;
-        while (true) {
-            Thread.sleep(500);
-            request = HttpRequest.newBuilder()
-                    .GET()
-                    .uri(URI.create("https://api.telegram.org/bot1263503443:AAG5Dz5XzWNjscLVjgl_kDEJqLx4zUlcJy8/getUpdates?offset=-1"))
-                    .build();
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            HttpHeaders headers = response.headers();
-            JsTelegram js = mapper.readValue(response.body(), JsTelegram.class);
-            System.out.println(js);
-
-            if (js.getResult().get(0).getUpdate_id() >= count) {
-
-                request = HttpRequest.newBuilder()
-                        .GET()
-                        .uri(URI.create("https://api.telegram.org/bot1263503443:AAG5Dz5XzWNjscLVjgl_kDEJqLx4zUlcJy8/sendMessage?chat_id=" +
-                                js.getResult().get(0).getMessage().getChat().getId() + "&text=hello"))
-                        .build();
-                response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-                count = js.getResult().get(0).getUpdate_id() + 1;
-            }
-            wait();
 
 
-//		 print status code
-//        System.out.println(response.statusCode());
-
-//		 print response body
-//        System.out.println(response.body());
-
-//
-
-        }
-    }
 }
 
 
