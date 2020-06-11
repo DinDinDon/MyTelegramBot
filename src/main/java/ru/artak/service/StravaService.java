0af00016ee5c311026e11f439f1374f361240026
ArtakMar <artak_mar.ru@mail.ru>
2020-06-12 01:49:25 +0300

    Поправил линии

--- src/main/java/ru/artak/service/StravaService.java
+++ src/main/java/ru/artak/service/StravaService.java
@@ -10,24 +10,24 @@ import java.io.IOException;
 
 
 public class StravaService {
-	
-	private final TelegramClient telegramClient;
-	private final StravaClient stravaClient;
-	private final String authorizedText = "Strava аккаунт был успешно подключен!";
-	private final Storage storage;
-	
-	public StravaService(TelegramClient telegramClient, Storage storage, StravaClient stravaClient) {
-		this.telegramClient = telegramClient;
-		this.storage = storage;
-		this.stravaClient = stravaClient;
-	}
-	
 
-	public void obtainCredentials(String state, String authorizationCode) throws IOException, InterruptedException {
-		Strava strava = stravaClient.getUpdateStrava(authorizationCode);
-		storage.saveStravaCredentials(storage.getChatIdByState(state),new StravaCredential(strava.getAccessToken(),strava.getRefreshToken()));
-		if(storage.getChatIdByState(state) != 0)
-		telegramClient.sendSimpleText(storage.getChatIdByState(state),authorizedText);
-	}
-	
+    private final TelegramClient telegramClient;
+    private final StravaClient stravaClient;
+    private final String authorizedText = "Strava аккаунт был успешно подключен!";
+    private final Storage storage;
+
+    public StravaService(TelegramClient telegramClient, Storage storage, StravaClient stravaClient) {
+        this.telegramClient = telegramClient;
+        this.storage = storage;
+        this.stravaClient = stravaClient;
+    }
+
+
+    public void obtainCredentials(String state, String authorizationCode) throws IOException, InterruptedException {
+        Strava strava = stravaClient.getUpdateStrava(authorizationCode);
+        storage.saveStravaCredentials(storage.getChatIdByState(state), new StravaCredential(strava.getAccessToken(), strava.getRefreshToken()));
+        if (storage.getChatIdByState(state) != 0)
+            telegramClient.sendSimpleText(storage.getChatIdByState(state), authorizedText);
+    }
+
 }
